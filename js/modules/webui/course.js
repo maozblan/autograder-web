6b7a0c08e8bb348821cf961f6ba83b84ae5fe32c
Eriq Augustine <eriq.augustine@gmail.com>
2025-02-17 20:02:51 -0800

    Added a simple rendering of assignment cards.

--- js/modules/webui/course.js
+++ js/modules/webui/course.js
@@ -4,13 +4,26 @@ import * as Routing from './routing.js'
 function init() {
     let requirements = {course: true};
     Routing.addRoute(/^courses$/, handlerCourses, 'Courses');
+    Routing.addRoute(/^course$/, handlerCourse, 'Course', {course: true});
 }
 
 function handlerCourses(path, params, context, container) {
     let cards = [];
     for (const [id, course] of Object.entries(context.courses)) {
-        let link = Routing.formHashPath('course', {id: course.id});
+        let link = Routing.formHashPath('course', {[Routing.PARAM_COURSE]: course.id});
         cards.push(Render.makeCardObject('course', course.name, link));
+    }
+
+    container.innerHTML = Render.cards(cards);
+}
+
+function handlerCourse(path, params, context, container) {
+    let course = context.courses[params[Routing.PARAM_COURSE]];
+
+    let cards = [];
+    for (const [id, assignment] of Object.entries(course.assignments)) {
+        let link = Routing.formHashPath('assignment', {[Routing.PARAM_ASSIGNMENT]: assignment.id});
+        cards.push(Render.makeCardObject('assignment', assignment.name, link));
     }
 
     container.innerHTML = Render.cards(cards);
