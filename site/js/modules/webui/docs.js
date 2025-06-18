d205fb4bdd15bb0cbe56418f81fa3f6c54b9e44c
maozblan <lyssa.li137@gmail.com>
2025-06-18 16:14:43 -0700

    Adding API Docs as a subpage to Server, fixing styles.

--- site/js/modules/webui/docs.js
+++ site/js/modules/webui/docs.js
@@ -3,7 +3,7 @@ import * as Render from "./render.js";
 import * as Routing from "./routing.js";
 
 function init() {
-    Routing.addRoute(/^docs$/, handlerDocs, "API Documentation");
+    Routing.addRoute(/^server\/docs$/, handlerDocs, "API Documentation");
 }
 
 function handlerDocs(path, params, context, container) {
@@ -14,12 +14,18 @@ function handlerDocs(path, params, context, container) {
             let html = `
                 <div class="api-docs">
                 	<div class="endpoints">
-                    	<input type="text" id="endpoint-filter" placeholder="Filter Endpoints">
-                        ${displayEndpoints(result.endpoints)}
+                        <h1>Endpoints</h1>
+                        <input type="text" id="endpoint-filter" placeholder="Filter Endpoints">
+                        <div class="scrollable">
+                            ${displayEndpoints(result.endpoints)}
+                        </div>
                     </div>
                 	<div class="types">
-                    	<input type="text" id="type-filter" placeholder="Filter Types">
-                        ${displayTypes(result.types)}
+                        <h1>Types</h1>
+                        <input type="text" id="type-filter" placeholder="Filter Types">
+                        <div class="scrollable">
+                            ${displayTypes(result.types)}
+                        </div>
                     </div>
                 </div>
             `;
@@ -28,7 +34,7 @@ function handlerDocs(path, params, context, container) {
 
             document.querySelector("#endpoint-filter").addEventListener("input", function(event) {
                 document.querySelectorAll(".endpoint").forEach(function(endpointDiv) {
-                    let tag = endpointDiv.getAttribute("data-tag");
+                    let tag = endpointDiv.getAttribute("data-endpoint");
                     if (tag.toLowerCase().includes(event.target.value.toLowerCase())) {
                         endpointDiv.style.display = "";
                     } else {
@@ -38,7 +44,7 @@ function handlerDocs(path, params, context, container) {
             });
             document.querySelector("#type-filter").addEventListener("input", function(event) {
                 document.querySelectorAll(".type").forEach(function(typeDiv) {
-                    let tag = typeDiv.getAttribute("data-tag");
+                    let tag = typeDiv.getAttribute("data-type");
                     if (tag.toLowerCase().includes(event.target.value.toLowerCase())) {
                         typeDiv.style.display = "";
                     } else {
@@ -53,10 +59,10 @@ function handlerDocs(path, params, context, container) {
 }
 
 function displayEndpoints(endpointData) {
-    let html = "<h1>Endpoints</h1>";
+    let html = "";
     Object.entries(endpointData).forEach(function([endpoint, data]) {
         html += `
-            <div class='endpoint' data-tag='${endpoint}'>
+            <div class='endpoint' data-endpoint='${endpoint}'>
                 <h3>${endpoint}</h3>
                 <p>${data.description}</p>
                 <div class="details">
@@ -88,10 +94,10 @@ function displayEndpoints(endpointData) {
 }
 
 function displayTypes(typeData) {
-    let html = "<h1>Types</h1>";
+    let html = ""
     Object.entries(typeData).forEach(function([type, data]) {
         html += `
-            <div class='type' data-tag='${type}'>
+            <div class='type' data-type='${type}'>
                 <h3>${type}</h3>
                 <p>${data.description ?? ""}</p>
                 <h4>Category</h4>
