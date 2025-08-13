cf50c0c485c9be81e474e773ba23091c26f3fb0e
maozblan <lyssa.li137@gmail.com>
2025-08-13 14:19:23 -0700

    Added test for server users/list page.

--- site/js/modules/webui/server.test.js
+++ site/js/modules/webui/server.test.js
@@ -27,3 +27,27 @@ test('Server Action', async function() {
     TestUtil.checkCards(expectedLabelNames);
 });
 
+test('Server Users List', async function() {
+    Base.init(false);
+    
+    await TestUtil.loginUser('server-admin');
+
+    let pathComponents = {
+        'path': Routing.PATH_SERVER_USERS_LIST,
+    };
+
+    let loadWaitPromise = Event.getEventPromise(Event.EVENT_TYPE_ROUTING_COMPLETE, pathComponents);
+
+    Routing.routeComponents(pathComponents);
+    await loadWaitPromise;
+
+    TestUtil.checkPageBasics('List Users', 'list users');
+
+    let resultWaitPromise = Event.getEventPromise(Event.EVENT_TYPE_TEMPLATE_RESULT_COMPLETE);
+    document.querySelector('.template-button').click();
+    await resultWaitPromise;
+
+    let results = document.querySelector('.results-area').innerHTML;
+    let userCount = results.matchAll('@test.edulinq.org').toArray().length;
+    expect(userCount).toEqual(9);
+});
