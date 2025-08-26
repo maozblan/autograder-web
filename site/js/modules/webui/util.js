d05a0a4bbd6159bc63856e5aef2deb1b469c5a19
Eriq Augustine <eriq.augustine@gmail.com>
2025-08-26 14:36:24 -0700

    Added some utilities.

--- site/js/modules/webui/util.js
+++ site/js/modules/webui/util.js
@@ -1,5 +1,7 @@
 import * as Event from './event.js';
 
+const WORD_BREAK_RE = /[\-_]+/
+
 let _testing = false;
 
 function setTesting(value) {
@@ -42,9 +44,53 @@ function downloadFile(file) {
     return true;
 }
 
+// Perform basic cleaning operations on a string destined for display.
+// WORD_BREAK_RE will be used to determine word breaks and converted to space.
+function cleanText(text) {
+    if (!text) {
+        return '';
+    }
+
+    // Replace word breaks.
+    text = text.replace(WORD_BREAK_RE, ' ');
+
+    // Replace any contiguous whitespace with a single space.
+    text = text.replace(/\s+/, ' ');
+
+    // Trim.
+    text = text.trim();
+
+    return text;
+}
+
+// Title case a string (with optional cleaning).
+// Words are tokenized based on whitespace (not regex word boundaries),
+// and rejoined with a single space.
+function titleCase(text, clean = true) {
+    if (!text) {
+        return '';
+    }
+
+    if (clean) {
+        text = cleanText(text);
+    }
+
+    return text.toLowerCase().split(/\s+/).map(function(word) {
+        return word.replace(word[0], word[0].toUpperCase());
+    }).join(' ');
+}
+
+// Check if a value is a JS object (and not an Array).
+function isObject(value) {
+    return (((typeof value) === "object") && !Array.isArray(value) && (value !== null));
+}
+
 export {
     caseInsensitiveStringCompare,
+    cleanText,
     downloadFile,
+    isObject,
     setTesting,
     timestampToPretty,
+    titleCase,
 }
--- site/js/modules/webui/util.test.js
+++ site/js/modules/webui/util.test.js
@@ -1,6 +1,7 @@
 import * as Util from './util.js'
 
-test("caseInsensitiveStringCompare base", function() {
+describe("Util.caseInsensitiveStringCompare() base", function() {
+    // [[a, b, expected], ...]
     const testCases = [
         ["A", "A", 0],
         ["a", "a", 0],
@@ -18,7 +19,92 @@ test("caseInsensitiveStringCompare base", function() {
         ["b", "A", 1],
     ];
 
-    testCases.forEach(function([a, b, expected]) {
+    test.each(testCases)("'%s' vs '%s'", function(a, b, expected) {
         expect(Util.caseInsensitiveStringCompare(a, b)).toBe(expected);
+    });
+});
+
+describe("Util.cleanText() base", function() {
+    // [[input, expected], ...]
+    const testCases = [
+        // Empty
+        ['', ''],
+
+        // Whitespace
+        [' ', ''],
+        [' \t\n\r ', ''],
+        [' a', 'a'],
+        ['a\n', 'a'],
+        [' a\t', 'a'],
+
+        // Word Breaks
+        ['a-b', 'a b'],
+        ['a_b', 'a b'],
+        ['a _b', 'a b'],
+        ['a_ b', 'a b'],
+        ['a _ b', 'a b'],
+    ];
+
+    test.each(testCases)("'%s'", function(input, expected) {
+        expect(Util.cleanText(input)).toBe(expected);
+    });
+});
+
+describe("Util.titleCase() base", function() {
+    // [[input, clean?, expected], ...]
+    const testCases = [
+        // Empty
+        ['', true, ''],
+
+        // Single Word
+        ['a', true, 'A'],
+        ['A', true, 'A'],
+        ['ab', true, 'Ab'],
+        ['AB', true, 'Ab'],
+
+        // Multiple Words
+        ['a b', true, 'A B'],
+        ['A B', true, 'A B'],
+        ['ab ab', true, 'Ab Ab'],
+        ['AB AB', true, 'Ab Ab'],
+
+        // Word Breaks
+        ['a-b', true, 'A B'],
+        ['a_b', true, 'A B'],
+        ['a \t b', true, 'A B'],
+
+        // Non-Word Breaks
+        ["a'b", true, "A'b"],
+
+        // No Clean
+        ['a-b', false, 'A-b'],
+    ];
+
+    test.each(testCases)("'%s', %s", function(input, clean, expected) {
+        expect(Util.titleCase(input, clean)).toBe(expected);
+    });
+});
+
+describe("Util.isObject() base", function() {
+    // [[input, expected], ...]
+    const testCases = [
+        // Non-Objetcs
+        [undefined, false],
+        [null, false],
+        ['', false],
+        ['a', false],
+        [1, false],
+
+        // Arrays
+        [[], false],
+        [[0], false],
+
+        // Objects
+        [{}, true],
+        [{a: 1}, true],
+    ];
+
+    test.each(testCases)("'%s'", function(input, expected) {
+        expect(Util.isObject(input)).toBe(expected);
     });
 });
